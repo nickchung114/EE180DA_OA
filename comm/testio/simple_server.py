2757f1281958a1fc2b951071a344db044ae16612
nickchung114 <nickchung114@gmail.com>
2017-02-02 16:49:14 -0800

    Fixed. Changed server-side firewall permissions.

--- comm/testio/simple_server.py
+++ comm/testio/simple_server.py
@@ -1,17 +1,22 @@
 import socket		# Import socket module
 import sys
-import threading
-import collections	# for Counter()
 
 HOST = socket.gethostname()	# Get local machine name
 PORT = 8000			# Reserve a port for your service
 
+print 'Making socket'
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)	# Create a socket object
+
+print 'Binding socket to port ', PORT
 s.bind((HOST,PORT))	# Bind to the port
-print 'Starting listen'
+
+print 'Listening...'
 s.listen(5)		# backlog = max # of queued connections; Waiting for connection
-print 'Starting listen'
+
+print 'Accepting...'
 c, addr = s.accept()	# Establish connection with client
+
 print 'Connected to ', addr	# WEEDLE
 data = int(c.recv(1))	# Receive id from client (e.g. 0x1F)
+
 print 'Connected with ID ', data	#WEEDLE
