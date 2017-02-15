255beb3e56b3e24dfb2d045bc06cadfa6449f580
Daguan <daguanchen@yahoo.com>
2017-02-15 21:59:22 +0000

    Dprintf causes 9DoF readings to become zero

--- comm/FOOTCLIENTW.c
+++ comm/FOOTCLIENTW.c
@@ -132,7 +132,15 @@ int main(int argc, char *argv[])
 			stompLastPlayed = 20;
 		}
 		printf("%f, %f, %f, %f, %f, %f\n", ax, ay, az, gx, gy, gz);
+		dprintf(client_socket_fd,"1\n");
+		printf("wrote to server\n");
+		if(stompLastPlayed > 0)
+		{
+			stompLastPlayed -= 1;
+		}
 		sleep(1/256);
 	}
+	// clean up the file descriptors
+	close(client_socket_fd);
 	return 0;
 }
