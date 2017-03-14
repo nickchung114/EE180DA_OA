8e1519f1b4a6822509cb22a9c59c1aa41e578a24
Kai Yang <kai.yang412@gmail.com>
2017-03-14 16:27:28 -0700

    stuff

--- comm/server/testingpy2mat.sh
+++ comm/server/testingpy2mat.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-matlab -nodisplay -nosplash -nodesktop -r "run('../../gait_tracking/Script_batched.m');exit;"
-#matlab -nodisplay -nosplash -nodesktop -r "run('Script_batched.m');exit;"
+gnome-terminal -e "matlab -nodisplay -nosplash -nodesktop -r \"run('../../gait_tracking/Script_batched.m');exit;\""
