1ae01def1d5da5eb1711f15d7789605636a50f94
Daguan <daguanchen@yahoo.com>
2017-03-15 01:14:15 +0000

    asdf

--- comm/handTest.c
+++ comm/handTest.c
@@ -17,7 +17,9 @@
 #define ACCEL_Z_OFFSET	1 
 #define ACCEL_Z_SCALE	1 //0.997808
 
-#define NUM_NOTES	5
+#define NUM_NOTES	7
+#define START_ANGLE	(-110)
+#define ANGLE_RANGE	220
 
 float update_run_avg(float *curr_avg, float num, int curr_avg_len) {
   *curr_avg = ((*curr_avg)*curr_avg_len + num)/(curr_avg_len+1);
@@ -50,27 +52,43 @@ int main(int argc, char *argv[]) {
 	accel = accel_init();
 	set_accel_scale(accel, a_scale);	
 	a_res = calc_accel_res(a_scale);
-
+	
+	//TODO include gyro stuff
+	gyro = gyro_init();
+	set_gyro_scale(gyro, g_scale);
+	g_res = calc_gyro_res(g_scale);
+	gyro_offset = calc_gyro_offset(gyro, g_res);	
 	//Wait for server pings.
 	//int continue = 1;
 	while (1) {
 		//code for classification.
 		accel_data = read_accel(accel, a_res);
-			
+		gyro_data = read_gyro(gyro, g_res);
+		
 		cali_xyz[0] = accel_data.x;
 		//cali_xyz[1] = accel_data.y;
 		cali_xyz[2] = accel_data.z;
 
 		x_angle = atan2(cali_xyz[2],cali_xyz[0]);
+		x_angle = -(x_angle*180/M_PI-90) + 5.5;
+		if (x_angle > 180) {
+			x_angle -= 360;
+		}
+		if (x_angle < START_ANGLE || x_angle > START_ANGLE + ANGLE_RANGE) {
+			printf("out of bounds\n");
+			usleep(1000000/10);
+			continue;
+		}
 		for(j = 1; j <= NUM_NOTES; j++) {
-			if(x_angle < -90 + j*180/NUM_NOTES) {
+			if(x_angle <= START_ANGLE + j*ANGLE_RANGE/NUM_NOTES) {
 				classify = j;
 				break;
 			}
 		}
-		printf("ACCX: %.3f, ACCY: %.3f, ACCZ: %.3f, ANGLE: %f,CLASS: %d\n",accel_data.x,accel_data.y,accel_data.z,x_angle, classify);
+		printf("GX: %.3f\tGY: %.3f\tGZ: %.3f\n", gyro_data.x,gyro_data.y,gyro_data.z);
+		//printf("ACCX: %.3f\tACCY: %.3f\tACCZ: %.3f\tANGLE: %f\tCLASS: %d\n",accel_data.x,accel_data.y,accel_data.z,x_angle, classify);
+		usleep(1000000/10);
 	}
-	// clean up the file descriptors
-	close(client_socket_fd);
 	return 0;
 }
+
